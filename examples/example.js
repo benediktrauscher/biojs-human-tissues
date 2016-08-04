e3ffb5b2c0ea817ecde3a43fc4e28124463dc8c1
BrennerT <t.brenner@dkfz-heidelberg.de>
2016-08-04 09:19:00 +0200

    added tissues lymph and the links in the example should now all work

--- examples/example.js
+++ examples/example.js
@@ -8,7 +8,7 @@
   tissuesmale.render();
 
   tissuesmale.addClickEvent('neck', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Neck','_blank');
+    window.open('https://en.wikipedia.org/wiki/Neck','_blank');
   });
 
   tissuesmale.addClickEvent('large_intestine', function(evt){
@@ -28,59 +28,67 @@
   });
 
   tissuesmale.addClickEvent('lungs', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Lungs','_blank');
+    window.open('https://en.wikipedia.org/wiki/Lungs','_blank');
   });
 
   tissuesmale.addClickEvent('stomach', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Stomach','_blank');
+    window.open('https://en.wikipedia.org/wiki/Stomach','_blank');
   });
 
   tissuesmale.addClickEvent('kidney', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Kidney','_blank');
+    window.open('https://en.wikipedia.org/wiki/Kidney','_blank');
   });
 
   tissuesmale.addClickEvent('prostate', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Prostate','_blank');
+    window.open('https://en.wikipedia.org/wiki/Prostate','_blank');
   });
 
   tissuesmale.addClickEvent('bladder', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Bladder','_blank');
+    window.open('https://en.wikipedia.org/wiki/Bladder','_blank');
   });
 
   tissuesmale.addClickEvent('bone', function(evt){
-    window.open('https://de.wikipedia.org/wiki/bone','_blank');
+    window.open('https://en.wikipedia.org/wiki/bone','_blank');
   });
 
   tissuesmale.addClickEvent('blood', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Blood','_blank');
+    window.open('https://en.wikipedia.org/wiki/Blood','_blank');
+  });
+
+  tissuesmale.addClickEvent('retina', function(evt){
+    window.open('https://en.wikipedia.org/wiki/Retina','_blank');
   });
 
   tissuesmale.addClickEvent('skin_man', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Skin','_blank');
+    window.open('https://en.wikipedia.org/wiki/Skin','_blank');
   });
 
   tissuesfemale.render();
 
   tissuesfemale.addClickEvent('brain', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Brain','_blank');
+    window.open('https://en.wikipedia.org/wiki/Brain','_blank');
   });
 
   tissuesfemale.addClickEvent('breast', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Breast','_blank');
+    window.open('https://en.wikipedia.org/wiki/Breast','_blank');
   });
 
   tissuesfemale.addClickEvent('cervix', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Cervix','_blank');
+    window.open('https://en.wikipedia.org/wiki/Cervix','_blank');
   });
 
   tissuesfemale.addClickEvent('ovary', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Ovary','_blank');
+    window.open('https://en.wikipedia.org/wiki/Ovary','_blank');
   });
 
   tissuesfemale.addClickEvent('uterus', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Uterus','_blank');
+    window.open('https://en.wikipedia.org/wiki/Uterus','_blank');
   });
 
   tissuesfemale.addClickEvent('skin_woman', function(evt){
-    window.open('https://de.wikipedia.org/wiki/Skin','_blank');
+    window.open('https://en.wikipedia.org/wiki/Skin','_blank');
+  });
+
+  tissuesfemale.addClickEvent('lymph', function(evt){
+    window.open('https://en.wikipedia.org/wiki/Lymph','_blank');
   });
